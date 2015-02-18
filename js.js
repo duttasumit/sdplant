aa087b14c6646c2e0eb99a6ccdf03a2881ee689a
Sumit Dutta <er.sumitdutta1991@gmail.com>
2015-02-18 16:45:17 +0530

    modified on feb 18

--- js.js
+++ js.js
@@ -1,8 +1,7 @@
-var cons_weighted_score, multiplier,
-student0 = 0, student1 = 0, student2 = 0;
-       
-$(document).ready(function(){  
-	
+$(document).ready(function(){
+	var cons_weighted_score, multiplier, student0 = 0, student1 = 0, student2 = 0;
+	var students=[], baskets=[]; 
+	const res=2,act=5, disc=5;
 	//function to show the tree image(size depends on value of student)
 	function tree(student,num){
 		if(student<=100){
@@ -12,6 +11,7 @@ $(document).ready(function(){
                 basket = parseInt((student - 80) / 20);
                 document.getElementById("container"+num).innerHTML = "<img src=' "+ Math.round(80 +( student % 20)) +".png'>";      
             }
+			return basket;
 	}
 			
 	//function to show number of baskets		
@@ -26,20 +26,17 @@ $(document).ready(function(){
 		}
 		if(basket<=Max){
 			$('#basket'+v +' .sum_of_basket').hide();
-		}
-		else {
+		}else {
 			basketminus3=basket-3;
 			$('#basket'+v +' .sum_of_basket').show();
 			$('#basket'+v +' .sum_of_basket  h3').html('and '+basketminus3+ ' more.');
-		}
-        
-    }
+		}}
 	
 	//these funtions will show the values of promptness, normalised score and login bonus 
 	function mainScore(){
-			$('#main_score span:first').html( Math.round((student0 - 4)/multiplier));
-            $('#main_score span:eq(1)').html(Math.round((student0-4)*100)/100);
-            $('#main_score span:eq(2)').html(Math.round(student0*100 )/100);
+			$('#main_score span:first').html( Math.round((students[0] - 4)/multiplier));
+            $('#main_score span:eq(1)').html(Math.round((students[0]-4)*100)/100);
+            $('#main_score span:eq(2)').html(Math.round(students[0]*100 )/100);
 	}
     
 	function st_Score(student,val,num){
@@ -80,10 +77,18 @@ $(document).ready(function(){
 			$('.right'+num+' .submission:not(:first), .right'+num+' .viewed:not(:first)').each(function(){
 					student += $(this).val()*multiplier;
 			});
-			console.log('student(login) : '+student);
+			return student;
 	}
 	
-	
+	//function for particular select
+	function part_sel(num,nxt){	
+			$('.inner-wrapper:last #delete').show();
+            $('#wrapper').append($('.inner-wrapper:last').clone());  
+            $('.inner-wrapper:last #delete').hide();
+            students[0] += num*multiplier;           
+            nxt.html(''+num+'');
+	}
+		
 	$('table tr:first-child').click(function(){
         $(this).siblings().toggle();	
     });
@@ -96,7 +101,7 @@ $(document).ready(function(){
     
 	//initial work(getting values)
     $('body').on('click','#go',function(){
-		var basket=0, basket0=0, basket1 = 0, basket2 = 0; 
+		var basket=0; var cont=0; 
         var num_of_resources = $('#resources').val();
         var num_of_activities = $('#activities').val();      
         var num_of_discussion = $('#discussions').val();      
@@ -104,115 +109,90 @@ $(document).ready(function(){
         if((num_of_activities>=0) && (num_of_resources>=0) && (num_of_discussion>=0)){ 
             $('#go').hide();  
             $('#wrapper').show();
-            $('#weighted_score_resources').text('2 X '+num_of_resources+' = '+2*num_of_resources);
-            $('#weighted_score_activities').text('5 X '+num_of_activities+' = '+5*num_of_activities);
-            $('#weighted_score_discussions').text('5 X '+num_of_discussion+' = '+5*num_of_discussion);
-            cons_weighted_score = (2*num_of_resources)+(5*num_of_activities)+(5*num_of_discussion);
+            $('#weighted_score_resources').text(''+res+' X '+num_of_resources+' = '+res*num_of_resources);
+            $('#weighted_score_activities').text(''+act+' X '+num_of_activities+' = '+act*num_of_activities);
+            $('#weighted_score_discussions').text(''+disc+' X '+num_of_discussion+' = '+disc*num_of_discussion);
+            cons_weighted_score = (res*num_of_resources)+(act*num_of_activities)+(disc*num_of_discussion);
             $('#weighted_score').text(cons_weighted_score);
-
             cons_weighted_score = cons_weighted_score * 75 / 100;
             $('#cons_weighted_score').text(cons_weighted_score); //+' ( 75% of ('+2*num_of_resources+' + '+ 5*num_of_activities+'))'
             $('#multiplier').text(Math.round((80/cons_weighted_score)*100)/100);
-
             multiplier = 80/cons_weighted_score;
             student0 = (multiplier*cons_weighted_score)/20;
-            console.log('student0 : '+student0);    
-            mainScore();
+            console.log('student0 : '+student0);
+			students.push(student0);
+			mainScore();
             $('#student1_score .span, .span1, .span2 ').html(0);
             $('#student2_score .span, .span1, .span2').html(0);
         }else{ alert("please enter positive values");}
-
-			tree(student0,0); basket0=basket;
-			tree(student1,1); basket2=basket;
-			tree(student2,2); basket1=basket;
-            Basket(basket0,'');
-            Basket(basket2,2);
-            Basket(basket1,1);
-
-    });
-   
+			students.push(student1);students.push(student2);			
+			for(cont in students){
+				tree(students[cont],cont);				
+				baskets[cont]=basket;
+				for(cont in baskets){
+				Basket(baskets[cont],cont);
+				cont++;}}
+	});
     $('#wrapper').append($('.inner-wrapper').eq(1).clone());
     $('.inner-wrapper:last').show();
     $('.inner-wrapper:last #delete').hide();    
 
 // delete particular
-    $('body').on("click","#delete:visible",function(){       
-        $(this).parent().parent().find('select:visible').each(function(){               
-            if(($(this).val()=='activity') || ($(this).val()=='discussion')){
-                student0 -= 5*multiplier;
+    $('body').on("click","#delete:visible",function(){     
+        $(this).parent().parent().find('select:visible').each(function(){
+			var value=$(this).val();		
+			if(($(this).val()=='activity') || ($(this).val()=='discussion')){
+                students[0] -= act*multiplier;
             }else if($(this).val()=='resource'){
-                student0 -= 2*multiplier;
-            }else if($(this).parent().hasClass('right1')){
-                student1 -= $(this).val()*multiplier;
-				var value=$(this).val();
-				studentScore(student1,value,1);                
-            }else if($(this).parent().hasClass('right2')){
-                student2 -= $(this).val()*multiplier;
-                var value1=$(this).val();
-				studentScore(student2,value1,2);            
-            }            
-        });
-			tree(student0,0); basket0=basket;
-			tree(student1,1); basket1=basket;
-			tree(student2,2); basket2=basket;
-            Basket(basket0,'');
-            Basket(basket2,2);
-            Basket(basket1,1);
+                students[0] -= res*multiplier;
+            }else{ ($(this).parent().attr("class")=="right1")?number=1:number=2;
+				students[number]-=value*multiplier;
+				studentScore(students[number],value,number);}
+			});
+			var cont=0;			
+			for(cont in students){
+				tree(students[cont],cont);				
+				baskets[cont]=basket;
+				for(cont in baskets){
+				Basket(baskets[cont],cont);
+				cont++;
+				}}
             mainScore();
        $(this).parent().parent().remove();    
     });
 
 // particular select
     $('body').on("change",".selected_particular",function(){
-        
-        if($(this).val()=='activity' || $(this).val()=='discussion'){
-            $('.inner-wrapper:last #delete').show();
-            $('#wrapper').append($('.inner-wrapper:last').clone());  
-            $('.inner-wrapper:last #delete').hide();
-            $(this).parent().siblings('.right1, .right2').find('[name="submission"]').show();
-            $(this).prop('disabled', true);
-            student0 += 5*multiplier;
-            console.log('student0 : '+student0);           
-            $(this).next().html('5');
+            var next=$(this).next();
+			if($(this).val()=='activity' || $(this).val()=='discussion'){            
+			part_sel(act,next);
+			$(this).parent().siblings('.right1, .right2').find('[name="submission"]').show();
+			$(this).prop('disabled', true);
         }else if($(this).val()=='resource'){
-            $('.inner-wrapper:last #delete').show();
-            $('#wrapper').append($('.inner-wrapper:last').clone());  
-            $('.inner-wrapper:last #delete').hide();
+			part_sel(res,next);
             $(this).parent().siblings('.right1, .right2').find('[name="viewed"]').show();
-            $(this).prop('disabled', true);
-            student0 += 2*multiplier;
-            console.log('student0 : '+student0);
-            $(this).next().html('2');
-        }else if($(this).val()=='new_week'){
+			$(this).prop('disabled', true);
+		}else if($(this).val()=='new_week'){
             $('<div class="new_week">New week started</div>').insertAfter( $('.inner-wrapper:last '));
             $('.inner-wrapper:last').clone().insertAfter($('.new_week:last'));  
             $(this).parent().parent().remove();             
         }
-			tree(student0,0);
-			basket0=basket;
-            Basket(basket0,'');
+			tree(students[0],0); baskets[0]=basket;
+            Basket(baskets[0],0);
             mainScore();
     });
    
 // login type and particular type
     $('body').on("change",".loginday, .submission,.viewed",function(){
-        if($(this).parent().hasClass('right1')){         
-			var value = $('.right1 .loginday').val();
-			login(value,1);          
-			student1=student;       
-			tree(student1,1);
-			basket1=basket;
-			Basket(basket1,1);
-            st_Score(student1,value,1);
-        }else{
-           var value = $('.right2 .loginday').val();
-            login(value,2);
-			student2=student;
-            tree(student2,2);
-			basket2=basket;               
-            Basket(basket2,2);
-            st_Score(student2,value,2);
-        }
+		var num;
+		($(this).parent().attr("class")=='right1')?num=1:num=2;
+		var value=$('.right'+num +' .loginday').val();
+		login(value,num);
+		students[num]=student;
+		tree(students[num],num);
+		baskets[num]=basket;
+		Basket(baskets[num],num);
+		st_Score(students[num],value,num);
         $(this).next().html($(this).val());
     });
 });        
